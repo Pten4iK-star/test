bffd84d480c93f3935976495860cbaa43a45a18d
Pten4iK-star <natalitsybenko777@gmail.com>
2020-02-27 19:36:51 +0200

    JavaScript2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,38 @@
 import "../styles/index.scss";
 
+const user = {
+  name: "Natalia",
+  lastName: "Tsybenko",
+  age: 34,
+  dateOfBirth: "07.09.1985",
+  hight: 170,
+  information: function() {
+    console.log("Natalia+Tsybenko+07.09.1985");
+  }
+};
+const HasAnymal = true;
+const notHasAnymal = false;
+console.log(typeof user);
+console.log(typeof HasAnymal);
+console.log(typeof notHasAnymal);
+const FevFood = {
+  sweeds: "ice cream",
+  candy: "chocolate",
+  cake: "biscuit"
+};
+const FevFood2 = {
+  first: "soup",
+  fixings: "pasta",
+  entree: "fish"
+};
+
+const myFevFoods = [FevFood, FevFood2];
+console.log(typeof myFevFoods);
+console.log(myFevFoods[0]);
+console.log(myFevFoods[1]);
+user.information();
+
+console.log("--------------------------");
 const a = 7;
 const b = 9;
 const sum = a + b;
