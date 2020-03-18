896a3bc9ec33f69f91b475693034e9a434c81507
Pten4iK-star <natalitsybenko777@gmail.com>
2020-03-18 18:18:21 +0200

    matrix (HTML+CSS)1 part

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,4 @@
 import "../styles/index.scss";
 import "./events";
-import "bootstrap";
-import "../scripts"
-const user = {
-  name: "Natalia",
-  lastName: "Tsybenko",
-  age: 34,
-  dateOfBirth: "07.09.1985",
-  height: 170,
-  hasAnimal: true,
-  notHasAnimal: false,
-  favoriteFood: ["pasta", "ice cream", "chocolate"],
-  getInformation: function() {
-    console.log(this.name + "  " + this.lastName + "  " + this.dateOfBirth);
-  }
-};
-console.log(user.height);
-console.log(user.notHasAnimal);
-console.log(user.hasAnimal);
-user.getInformation();
-console.log(user.favoriteFood);
-
-console.log("--------------------------");
-const a = 7;
-const b = 9;
-const sum = a + b;
-console.log(sum);
-console.log("const", a - b, a * b, a / b);
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../scripts";
